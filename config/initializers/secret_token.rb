0c6f040bc346ba66ef96da286eefda1126e2ff19
hui <hui@networking.io>
2013-04-28 23:19:29 +0800

    init rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AngularjsScaffoldOnRails4::Application.config.secret_key_base = '4274d1032b1ec170f2f8d63c90109b516bc9eefe7db2491435afdfa65c4f179e2561a1a75e9c2bdf1d2cbec69284ee6df76a140585339bb55ec4da8f10197e9c'
